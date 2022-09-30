644e9c0f1a17fa391e90e7ad2fa7f7acfc144bba
ceoconn <coconnell1661@gmail.com>
2022-09-30 08:41:22 -0600

    added api folder and files, almost finished with jb part 2/part 1

---
+++ api/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+usersRouter.use((req, res, next) => {
+    console.log('Request being made to /users');
+
+    res.send({ message: 'hello from /users' });
+});
+
+
+
+module.exports = usersRouter;
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 
-
-
-
 const PORT = 3000;
 const express = require('express');
 const server = express();
+
+const morgan = require('morgan');
+// morgan('dev') is a function that logs incoming requests
+// logs the method, route, HTTP response code and time it took to form
+server.use(morgan('dev'));
+
+// express.json() is a function that reads incoming JSON from requests
+// requires request's header to "Content-Type: application/json"
+server.use(express.json());
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
